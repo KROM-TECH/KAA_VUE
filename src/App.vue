d0ef0dc8b912e3a3f8c8faaf8946a01a276cfff8
kromate <akpananthony33@gmail.com>
2020-09-13 18:38:06 +0100

    initial-commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-view />
+</template>
+
+<style>
+  * {
+    font-family: HKGroteskPro, serif;
+  }
+.modal-header {
+  border-bottom: none !important;
+  padding: 0rem 1rem !important;
+}
+.modal-body {
+    position: relative;
+    flex: 1 1 auto;
+    padding: 0rem 1rem 1rem 1rem !important;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Book from "../views/Book.vue";
+
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/",
+    name: "Book",
+    component: Book
+  },
+ 
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
