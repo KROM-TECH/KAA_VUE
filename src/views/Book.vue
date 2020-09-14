350e6f559ae9a6788b8b90f8f8494d57cd08077d
kromate <akpananthony33@gmail.com>
2020-09-14 18:27:26 +0100

    firebase

--- src/views/Book.vue
+++ src/views/Book.vue
@@ -13,7 +13,7 @@
 
         <b-row align-h="center" class="my-3 px-2">
           <b-input-group>
-            <b-form-input :placeholder="placeholder" :disabled="selected == 'Search by'"></b-form-input>
+            <b-form-input :placeholder="placeholder" v-model="searchInput" :disabled="selected == 'Search by'"></b-form-input>
 
             <template v-slot:append>
               <b-dropdown text="Search By" class="btn-primary-soft" variant="outline-success">
@@ -27,7 +27,7 @@
                 <b-dropdown-item @click="dropdown('Faculty')">Faculty</b-dropdown-item>
                 <b-dropdown-item @click="dropdown('Department')">Department</b-dropdown-item>
               </b-dropdown>
-              <b-button class="btn-primary-soft" variant="outline-primary">Search</b-button>
+              <b-button class="btn-primary-soft" variant="outline-primary" :disabled="selected == 'Search by' || searchInput == ''" @click="searchButton">Search</b-button>
             </template>
           </b-input-group>
         </b-row>
@@ -61,12 +61,14 @@
   import 'firebase/auth';
   import 'firebase/firestore';
   import 'firebase/storage';
+
   export default {
     data() {
       return {
         NumBook: '',
         selected: 'Search by',
-        placeholder: ''
+        placeholder: '',
+        searchInput: ''
       };
     },
 
@@ -74,47 +76,103 @@
       dropdown(x) {
         this.selected = x;
       },
-      loadTable() {
+      loadTableData(querySnapshot) {
         const table = document.querySelector('.tbodyData');
+        const storageReference = firebase.storage().ref();
+        querySnapshot.forEach((doc) => {
+          const document = doc.data();
+          storageReference
+            .child('books/' + `${document.book}`)
+            .getDownloadURL()
+            .then((url) => {
+              let tableRow = ``;
+              tableRow += `<tr class="rolling" >`;
+              tableRow += `<td class="book"><a href=${url}; download=${document.bookName} target='_blank'><i class="fa fa-arrow-down"  style="color:green"></i> </a></td>`;
+              tableRow += `<td class="bookName"> ${document.bookName}</td>`;
+              tableRow += `<td class="bookAuthor"> ${document.bookAuthor}</td>`;
+              tableRow += `<td class="university"> ${document.university}</td>`;
+              tableRow += `<td class="faculty"> ${document.faculty} </td>`;
+              tableRow += `<td class="department"> ${document.department} </td>`;
+              tableRow += `<td class="level"> ${document.level} </td>`;
+              tableRow += `<td class="semester"> ${document.semester} </td>`;
+              tableRow += `<td class="uploader"> ${document.uploader}</td>`;
+              tableRow += `</tr>`;
+              table.innerHTML += tableRow;
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        });
+      },
+      initTable() {
         firebase
           .firestore()
           .collection('books')
           .get()
           .then((querySnapshot) => {
             this.NumBook = querySnapshot.size;
-            const storageReference = firebase.storage().ref();
-            querySnapshot.forEach((doc)=> {
-              const document = doc.data();
-              storageReference
-                .child('books/' + `${document.book}`)
-                .getDownloadURL()
-                .then((url) => {
-                  let tableRow = ``;
-
-                  tableRow += `<tr class="rolling" >`;
-                 tableRow += `<td class="book"><a href=${url}; download=${document.bookName}><i class="fa fa-arrow-down"  style="color:green"></i> </a></td>`;
-                  tableRow += `<td class="bookName"> ${document.bookName}</td>`;
-                  tableRow += `<td class="bookAuthor"> ${document.bookAuthor}</td>`;
-                  tableRow += `<td class="university"> ${document.university}</td>`;
-                  tableRow += `<td class="faculty"> ${document.faculty} </td>`;
-                  tableRow += `<td class="department"> ${document.department} </td>`;
-                  tableRow += `<td class="level"> ${document.level} </td>`;
-                  tableRow += `<td class="semester"> ${document.semester} </td>`;
-                  tableRow += `<td class="uploader"> ${document.uploader}</td>`;
-                  tableRow += `</tr>`;
-
-                  table.innerHTML += tableRow;
-                })
-                             .catch((error) => {
-                  console.log(error);
-                });
-            })
+            this.loadTableData(querySnapshot);
           });
+      },
+      searchButton() {
+        document.querySelector('.tbodyData').innerHTML = '';
+        const booksRef = firebase.firestore().collection('books');
+        const searchValue = this.searchInput;
+
+        if (this.selected == 'Uploader') {
+          booksRef
+            .orderBy('uploader')
+            .startAt(searchValue.toLowerCase())
+            .endAt(searchValue.toLowerCase() + '\uf8ff')
+            .onSnapshot((querySnapshot) => {
+              this.loadTableData(querySnapshot);
+            });
+        } else if (this.selected == 'University') {
+          booksRef
+            .orderBy('university')
+            .startAt(searchValue.toLowerCase())
+            .endAt(searchValue.toLowerCase() + '\uf8ff')
+            .onSnapshot((querySnapshot) => {
+              this.loadTableData(querySnapshot);
+            });
+        } else if (this.selected == 'Book Name') {
+          booksRef
+            .orderBy('bookName')
+            .startAt(searchValue.toLowerCase())
+            .endAt(searchValue.toLowerCase() + '\uf8ff')
+            .onSnapshot((querySnapshot) => {
+              this.loadTableData(querySnapshot);
+            });
+        } else if (this.selected == 'Book Author') {
+          booksRef
+            .orderBy('bookAuthor')
+            .startAt(searchValue.toLowerCase())
+            .endAt(searchValue.toLowerCase() + '\uf8ff')
+            .onSnapshot((querySnapshot) => {
+              this.loadTableData(querySnapshot);
+            });
+        } else if (this.selected == 'Faculty') {
+          booksRef
+            .orderBy('faculty')
+            .startAt(searchValue.toLowerCase())
+            .endAt(searchValue.toLowerCase() + '\uf8ff')
+            .onSnapshot((querySnapshot) => {
+              this.loadTableData(querySnapshot);
+            });
+        } else if (this.selected == 'Department') {
+          booksRef
+            .orderBy('department')
+            .startAt(searchValue.toLowerCase())
+            .endAt(searchValue.toLowerCase() + '\uf8ff')
+            .onSnapshot((querySnapshot) => {
+              this.loadTableData(querySnapshot);
+            });
+        }
       }
     },
 
     mounted() {
-      this.loadTable();
+      this.initTable();
     }
   };
 </script>
