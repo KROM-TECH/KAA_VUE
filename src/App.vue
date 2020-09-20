03e2821312979d4c379c01a58b2284075484446c
kromate <akpananthony33@gmail.com>
2020-09-20 20:14:54 +0100

    book

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@
 </template>
 
 <style>
+.table th, .table td {
+    padding: 0.5rem !important;
+    vertical-align: middle !important;
+    border-top: 1px solid #dee2e6;
+    font-family: 'Work Sans', sans-serif !important;
+}
 @import url('https://fonts.googleapis.com/css2?family=Work+Sans:ital@1&display=swap');
 *{
   padding: 0;
@@ -26,4 +32,60 @@ div#modal-prevent-closing{
 form.form{
 font-family: 'Work Sans', sans-serif !important;
 }
+  .btn-success-soft {
+    background-color: rgba(66, 186, 150, 0.1);
+    color: #42ba96;
+  }
+  .btn-success-soft:hover {
+    background-color: rgba(66, 186, 150, 0.3);
+    color: #42ba96;
+  }
+  .btn-success-soft:focus {
+    background-color: rgba(66, 186, 150, 0.3);
+    color: #42ba96;
+    border-color: transparent;
+    box-shadow: none;
+  }
+  .btn-info-soft {
+    background-color: rgba(124, 105, 239, 0.1);
+    color: #7c69ef;
+  }
+  .btn-info-soft:hover {
+    background-color: rgba(124, 105, 239, 0.3);
+    color: #7c69ef;
+  }
+  .btn-info-soft:focus {
+    background-color: rgba(124, 105, 239, 0.3);
+    color: #7c69ef;
+    border-color: transparent;
+    box-shadow: none;
+  }
+  .btn-primary-soft {
+    background-color: rgba(51, 94, 234, 0.1);
+    color: #335eea;
+  }
+  .btn-primary-soft:hover {
+    background-color: rgba(51, 94, 234, 0.3);
+    color: #335eea;
+  }
+  .btn-primary-soft:focus {
+    background-color: rgba(51, 94, 234, 0.3);
+    color: #335eea;
+    border-color: transparent;
+    box-shadow: none;
+  }
+  .btn-secondary-soft {
+    background-color: #d34f4233;
+    color: #d34f42;
+  }
+  .btn-secondary-soft:hover {
+    background-color: #d34f4270;
+    color: #d34f42;
+  }
+  .btn-secondary-soft:focus {
+    background-color: #d34f4270;
+    color: #d34f42;
+    border-color: transparent;
+    box-shadow: none;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,15 +4,40 @@ import router from "./router";
 import store from "./store";
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/analytics';
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDGPuiAwA6-BkL-r7qGsWNEpxucW-3TXjM",
+  authDomain: "kromtech-archive.firebaseapp.com",
+  databaseURL: "https://kromtech-archive.firebaseio.com",
+  projectId: "kromtech-archive",
+  storageBucket: "kromtech-archive.appspot.com",
+  messagingSenderId: "147947718015",
+  appId: "1:147947718015:web:c1afcf6e27d8a3a8e2c64a",
+  measurementId: "G-7C2TXMJXZR"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: (h) => h(App)
+    }).$mount('body');
+  }
+});
