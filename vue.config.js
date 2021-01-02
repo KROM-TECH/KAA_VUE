f43c3aaf894d0b5c8b207abc8f998ca2e450a935
kromate <akpananthony33@gmail.com>
2021-01-02 17:02:32 +0100

    git

--- vue.config.js
+++ vue.config.js
@@ -24,10 +24,6 @@ module.exports = {
     //   },
       // configure the workbox plugin
       workboxPluginMode: 'GenerateSW',
-      workboxOptions: {
-        // swSrc is required in InjectManifest mode.
-        swSrc: 'dev/sw.js',
-        // ...other Workbox options...
-      }
+  
     }
   }
