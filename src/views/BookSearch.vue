368831a1481dbdf8989537ea3d23bd4d1fc753c7
kromate <akpananthony33@gmail.com>
2021-01-30 12:34:08 +0100

    search added

--- src/views/BookSearch.vue
+++ src/views/BookSearch.vue
@@ -25,14 +25,9 @@
             :placeholder="placeholder"
             v-model="searchInput"
             type="search"
-            :disabled="selected == ''"
           ></b-form-input>
 
-          <button
-            class="primary my-3 mt-3"
-            :disabled="selected == '' || searchInput == ''"
-            @click="searchButton"
-          >
+          <button class="primary my-3 mt-3" :disabled="searchInput == ''" @click="searchButton">
             Search
           </button>
         </b-row>
@@ -57,215 +52,39 @@
 
 <script>
 import Error from "@/components/Error.vue";
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-import "firebase/storage";
 import Menu from "@/components/Menu.vue";
 import NotFound from "@/components/404.vue";
-
-import faculties from "@/helpers/faculties.js";
-import universities from "@/helpers/universities.js";
-import level from "@/helpers/level.js";
 
 export default {
   components: { Menu, Error, NotFound },
   data() {
     return {
-      universities,
-      faculties,
-      level,
-
       empty: false,
       Error: false,
-      searchResult: "",
       placeholder: "",
       searchInput: "",
       data: [],
-      options: [
-        { value: "", text: "Search by", disabled: true },
-        { value: "Uploader", text: "Uploader" },
-        { value: "University", text: "University" },
-        { value: "Book Name", text: "Book Name" },
-        { value: "Book Author", text: "Book Author" },
-        { value: "Faculty", text: "Faculty" },
-        { value: "Department", text: "Department" },
-        { value: "Level", text: "Level" },
-      ],
       modalShow: false,
       loading: true,
-      headers: [
-        { text: "Dw", value: "url", width: 4 },
-        { text: "Book Name", value: "BN", width: 12 },
-        { text: "Book Author", value: "BA", width: 12 },
-        { text: "University", value: "Uni", width: 12 },
-        { text: "Faculty", value: "Fac", width: 12 },
-        { text: "Department", value: "Dep", width: 12 },
-        { text: "Level", value: "Lev", width: 12 },
-        { text: "Uploader", value: "Upl", width: 12 },
-      ],
     };
   },
   computed: {
-    availableView() {
+    searchResult() {
       return this.NumBook > 15 ? "15" : this.NumBook;
-    },
-  },
-  filters: {
-    truncate: function(text, length = 24, clamp) {
-      clamp = clamp || "...";
-      var node = document.createElement("div");
-      node.innerHTML = text;
-      var content = node.textContent;
-      return content.length > length ? content.slice(0, length) + clamp : content;
     },
   },
 
   methods: {
-    dropdown(x) {
-      this.selected = x;
-    },
-    loadTableData(querySnapshot) {
-      const storageReference = firebase.storage().ref();
-      querySnapshot.forEach((doc) => {
-        const document = doc.data();
-        storageReference
-          .child("books/" + `${document.book}`)
-          .getDownloadURL()
-          .then((url) => {
-            console.log(document);
-            const content = {
-              url: url,
-              BN: document.bookName,
-              BA: document.bookAuthor,
-              Uni: document.university,
-              Fac: document.faculty,
-              Dep: document.department,
-              Lev: document.level,
-              Upl: document.uploader,
-            };
-            console.log(content);
-            this.data.push(content);
-          }, (this.loading = false))
-          .catch((error) => {
-            console.log(error);
-            this.Error = true;
-          });
-      });
-    },
-    initTable() {
-      firebase
-        .firestore()
-        .collection("books")
-        .get()
-        .then((querySnapshot) => {
-          this.NumBook = querySnapshot.size;
-          this.loadTableData(querySnapshot);
-        })
-        .catch((error) => {
-          console.log(error);
-          this.Error = true;
-        });
-    },
-    searchButton() {
-      this.data = [];
-      this.empty = false;
-
-      const booksRef = firebase.firestore().collection("books");
-      const searchValue = this.searchInput;
-
-      if (this.selected == "Uploader") {
-        booksRef
-          .orderBy("uploader")
-          .startAt(searchValue)
-          .endAt(searchValue + "\uf8ff")
-          .onSnapshot((querySnapshot) => {
-            if (!querySnapshot.empty) {
-              this.loadTableData(querySnapshot);
-            } else {
-              this.empty = true;
-            }
-          });
-      } else if (this.selected == "University") {
-        booksRef
-          .orderBy("university")
-          .startAt(searchValue)
-          .endAt(searchValue + "\uf8ff")
-          .onSnapshot((querySnapshot) => {
-            if (!querySnapshot.empty) {
-              this.loadTableData(querySnapshot);
-            } else {
-              this.empty = true;
-            }
-          });
-      } else if (this.selected == "Book Name") {
-        booksRef
-          .orderBy("bookName")
-          .startAt(searchValue)
-          .endAt(searchValue + "\uf8ff")
-          .onSnapshot((querySnapshot) => {
-            if (!querySnapshot.empty) {
-              this.loadTableData(querySnapshot);
-            } else {
-              this.empty = true;
-            }
-          });
-      } else if (this.selected == "Book Author") {
-        booksRef
-          .orderBy("bookAuthor")
-          .startAt(searchValue)
-          .endAt(searchValue + "\uf8ff")
-          .onSnapshot((querySnapshot) => {
-            if (!querySnapshot.empty) {
-              this.loadTableData(querySnapshot);
-            } else {
-              this.empty = true;
-            }
-          });
-      } else if (this.selected == "Faculty") {
-        booksRef
-          .orderBy("faculty")
-          .startAt(searchValue)
-          .endAt(searchValue + "\uf8ff")
-          .onSnapshot((querySnapshot) => {
-            if (!querySnapshot.empty) {
-              this.loadTableData(querySnapshot);
-            } else {
-              this.empty = true;
-            }
-          });
-      } else if (this.selected == "Department") {
-        booksRef
-          .orderBy("department")
-          .startAt(searchValue)
-          .endAt(searchValue + "\uf8ff")
-          .onSnapshot((querySnapshot) => {
-            if (!querySnapshot.empty) {
-              this.loadTableData(querySnapshot);
-            } else {
-              this.empty = true;
-            }
-          });
-      } else if (this.selected == "Level") {
-        booksRef
-          .orderBy("level")
-          .startAt(searchValue)
-          .endAt(searchValue + "\uf8ff")
-          .onSnapshot((querySnapshot) => {
-            if (!querySnapshot.empty) {
-              this.loadTableData(querySnapshot);
-            } else {
-              this.empty = true;
-            }
-          });
-      }
-
-      this.loading = false;
+    search() {
+      fetch("https://book-web-scraper-api.herokuapp.com/search/?name=sadiku")
+        .then((response) => response.json())
+        .then((data) => (this.data = data))
+        .catch(console.log());
     },
   },
 
   created() {
-    // this.initTable();
+    this.search();
   },
 };
 </script>
