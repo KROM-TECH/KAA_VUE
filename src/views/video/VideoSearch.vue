9c2ef79ce781319956b98cbb27f3862b28d111df
kromate <akpananthony33@gmail.com>
2021-02-10 17:44:31 +0100

    started the video stuff

---
+++ src/views/video/VideoSearch.vue
@@ -0,0 +1,225 @@
+<template>
+  <div>
+    <Error v-if="Error" @close="Error = !Error" />
+    <Menu />
+    <section class="pt-4 pt-md-11">
+      <b-container>
+        <b-row align-h="center" class="font text-center">
+          <b-col>
+            <h1>
+              <span class="tex-primary">Book Search</span>
+            </h1>
+            <p class="lead text-gray-700">
+              Use this Option if you have a particular book you are looking for.
+              <br />
+              <!-- <span class="result" v-if="data.length" v-html="searchResult"></span> -->
+              <br />
+              <router-link class="box" to="/guide" style="margin: 1rem;">
+                Learn More
+              </router-link>
+            </p>
+          </b-col>
+        </b-row>
+
+        <b-row class="justify-content-center ">
+          <b-form-input
+            class="input my-3"
+            :placeholder="placeholder"
+            v-model="searchInput"
+            type="search"
+          ></b-form-input>
+
+          <button
+            class="primary my-3 mt-3"
+            :disabled="searchInput == ''"
+            @click="search"
+            @keypress.enter="search"
+          >
+            Search
+          </button>
+        </b-row>
+
+        <div style="overflow-x: auto;">
+          <NotFound v-if="Error" />
+          <div class="d-flex justify-content-center" v-if="loading">
+            <b-spinner type="grow load" label="Loading..."></b-spinner>
+          </div>
+
+          <h6
+            class="list"
+            v-for="(n, index) in data"
+            :key="index"
+            v-else
+            @click="openDetails(n.link)"
+          >
+            {{ n.name }}
+          </h6>
+        </div>
+      </b-container>
+    </section>
+
+    <BookSearchModal :showModal="showModal" @close="showModal = !showModal" :link="link" />
+  </div>
+</template>
+
+<script>
+import Error from "@/components/Error.vue";
+import Menu from "@/components/Menu.vue";
+import NotFound from "@/components/404.vue";
+import BookSearchModal from "@/components/modals/BookSearchModal.vue";
+
+export default {
+  components: { Menu, Error, NotFound, BookSearchModal },
+  data() {
+    return {
+      showModal: false,
+      loading: false,
+      Error: false,
+      link: "",
+      placeholder: "",
+      searchInput: "",
+      data: [],
+      modalShow: false,
+    };
+  },
+  computed: {
+    searchResult() {
+      return `your search <span class='edit'> ${this.searchInput}</span> return <span class='edit'>${this.data.length}</span> result`;
+    },
+  },
+
+  methods: {
+    openDetails(link) {
+      this.link = link;
+      this.showModal = true;
+    },
+    search() {
+      this.Error = false;
+      this.loading = true;
+
+      fetch(
+        encodeURI(
+          `https://us-central1-kromtech-archive.cloudfunctions.net/BookScrape/?name=${this.searchInput}`,
+        ),
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          this.data = data;
+          this.loading = false;
+        })
+        .catch((err) => {
+          console.log(err);
+          this.Error = true;
+        });
+    },
+  },
+
+  // created() {
+  //   this.search();
+  // },
+};
+</script>
+
+<style scoped>
+.result {
+  font-weight: 700;
+  font-size: normal;
+  text-transform: capitalize;
+  color: #00276f;
+}
+.edit {
+  color: #051127;
+  text-decoration-line: underline;
+}
+.list {
+  border-radius: 5px;
+  background: #5976ac;
+  padding: 10px;
+  text-align: center;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border: 1px solid #00276f;
+  cursor: pointer;
+}
+.img-fluid {
+  height: 20px;
+  width: 20px;
+}
+.input {
+  width: 60% !important;
+  background: #ffffff7a;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  margin: 0px 9px;
+  margin-right: 3rem;
+  font-weight: 550;
+}
+.form-control {
+  width: 60% !important;
+  background: #ffffff7a;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  margin: 0px 9px;
+  margin-right: 3rem;
+  font-weight: 550;
+}
+@media (max-width: 1000px) {
+  .form-control {
+    width: 80% !important;
+    background: #ffffff7a;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    margin: 0px 9px;
+    /* margin-right: 3rem; */
+  }
+
+  .mt-3 {
+    margin-top: 0rem !important;
+  }
+}
+.btn-outline-success {
+  color: #fff !important;
+}
+.soft {
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  color: #fff !important;
+  border-color: #1a6b2d;
+  /* background: #488b89; */
+  width: 9rem;
+  height: 2.4rem;
+  border-radius: 0.2rem;
+  font-weight: 600;
+}
+.primary,
+.btn-secondary {
+  background-color: #00276f !important;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  color: #fff !important;
+  width: 9rem;
+  height: 2.4rem;
+  border-radius: 0.2rem;
+  font-weight: 600;
+}
+.primary:hover,
+.btn-secondary:hover {
+  background: #00276f94 !important;
+  border-color: #00276f94 !important;
+}
+
+.spinner {
+  margin: 10px auto 0;
+  width: 70px;
+  text-align: center;
+}
+a {
+  color: #00276f !important;
+  cursor: pointer;
+}
+.tex-primary {
+  color: #00276f !important;
+  font-weight: 600;
+  font-size: 2.5rem;
+}
+.load {
+  background-color: #00276f !important;
+  height: 3rem !important;
+  width: 3rem !important;
+}
+</style>
---
+++ src/views/video/index.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <Menu />
+    <section class="pt-4 pt-md-11">
+      <b-container>
+        <b-row align-h="center" class="font text-center">
+          <b-col>
+            <h1>
+              <span class="tex-primary">Books</span>
+            </h1>
+            <p class="lead text-gray-700">
+              Select your Option below <br />Read the guide
+              <router-link to="/guide"> here </router-link> if you are uncertain on what to select
+            </p>
+          </b-col>
+        </b-row>
+
+        <b-row class="justify-content-center align-items-center">
+          <router-link to="/bookindex">
+            <b-card class="card"><h4>Indexed Search</h4></b-card>
+          </router-link>
+          <router-link to="/booksearch">
+            <b-card class="card"><h4>Worldwide Search</h4></b-card>
+          </router-link>
+        </b-row>
+      </b-container>
+    </section>
+  </div>
+</template>
+
+<script>
+import Menu from "@/components/Menu.vue";
+
+export default {
+  components: { Menu },
+  data() {
+    return {
+      NumBook: "",
+      selected: "Type",
+      placeholder: "",
+      searchInput: "",
+      data: [],
+      loading: true,
+    };
+  },
+
+  methods: {
+    dropdown(x) {
+      this.selected = x;
+    },
+  },
+
+  mounted() {},
+};
+</script>
+
+<style scoped>
+.header {
+  font-size: 1.3rem;
+  font-weight: 800;
+}
+h1 {
+  font-weight: 550 !important;
+}
+.card {
+  text-align: center;
+  max-width: 90vw;
+  margin-bottom: 1rem;
+  width: 600px;
+}
+.lead {
+  color: rgba(0, 0, 0, 0.726) !important;
+  font-weight: 540;
+  font-size: 1.1rem;
+  padding: 1rem;
+  word-spacing: 2px;
+  line-height: 1.5rem;
+}
+.soft {
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  color: #fff !important;
+  border-color: #1a6b2d;
+  /* background: #488b89; */
+  width: 9rem;
+  height: 2.4rem;
+  border-radius: 0.2rem;
+  font-weight: 600;
+}
+.upload {
+  color: #00276f !important;
+  text-align: center;
+  font-weight: 550 !important;
+}
+.tex-primary {
+  color: #00276f !important;
+  font-weight: 600;
+  font-size: 2.5rem;
+}
+</style>
